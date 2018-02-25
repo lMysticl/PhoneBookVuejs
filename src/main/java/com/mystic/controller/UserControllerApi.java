92b994124c9e2d61548e7043604514810a647df1
lMysticl <putrenkov99@gmail.com>
2018-02-25 06:51:39 +0200

    -fix validation

--- src/main/java/com/mystic/controller/UserControllerApi.java
+++ src/main/java/com/mystic/controller/UserControllerApi.java
@@ -10,8 +10,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 /**
  * @author Putrenkov Pavlo
  */
@@ -30,7 +28,7 @@ public class UserControllerApi {
             @ApiResponse(code = 403, message = "Access denied"),
             @ApiResponse(code = 404, message = "The user doesn't exist"),
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public void register(@Valid @RequestBody String data) throws RegistrationException {
+    public void register(@RequestBody String data) throws RegistrationException {
         User user = getUserService.getUserService(data);
         userServiceImpl.save(user);
     }
