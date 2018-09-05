f9b1c05a2ef27d7b4291e36e364666bc29611f09
Pavlo Putrenkov <putrenkov99@gmail.com>
2018-09-06 01:03:12 +0300

    -fix null in avatar update

--- src/main/java/com/mystic/validation/ValidationRegistration.java
+++ src/main/java/com/mystic/validation/ValidationRegistration.java
@@ -3,6 +3,7 @@ package com.mystic.validation;
 import com.mystic.exceptions.RegistrationException;
 import com.mystic.model.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,7 +21,6 @@ public class ValidationRegistration {
 
     public void validation(String firstname, String username) throws RegistrationException {
 
-
         if (!validationService.isNameValid(firstname) & !firstname.equals("")) {
             throw new RegistrationException(firstname + "Firstname is not valid");
         }
@@ -29,9 +29,11 @@ public class ValidationRegistration {
             throw new RegistrationException("Username is not valid");
         }
 
-        if (userRepository.findByUsername(username) != null ) {
+        if(SecurityContextHolder.getContext().getAuthentication()!=null){
+        String name = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userRepository.findByUsername(username) != null && !name.equals(username)) {
             throw new RegistrationException(username + " User exist");
-        }
+        }}
     }
 
 }
