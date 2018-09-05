c3ed821ed8b98b5fcf1a45e09f0c7fbdce2ece79
Pavlo Putrenkov <putrenkov99@gmail.com>
2018-09-06 00:26:21 +0300

    -fix error registration

--- src/main/java/com/mystic/validation/ValidationRegistration.java
+++ src/main/java/com/mystic/validation/ValidationRegistration.java
@@ -3,7 +3,6 @@ package com.mystic.validation;
 import com.mystic.exceptions.RegistrationException;
 import com.mystic.model.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,9 +18,8 @@ public class ValidationRegistration {
 
     private ValidationService validationService;
 
-    public void validation(String firstname,  String username) throws RegistrationException {
+    public void validation(String firstname, String username) throws RegistrationException {
 
-        String name = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         if (!validationService.isNameValid(firstname) & !firstname.equals("")) {
             throw new RegistrationException(firstname + "Firstname is not valid");
@@ -31,7 +29,7 @@ public class ValidationRegistration {
             throw new RegistrationException("Username is not valid");
         }
 
-        if (userRepository.findByUsername(username) != null && !name.equals(username)) {
+        if (userRepository.findByUsername(username) != null ) {
             throw new RegistrationException(username + " User exist");
         }
     }
