2fef5d6fb410c94a5dc1b4fe313c921b544e151d
Pavlo Putrenkov <putrenkov99@gmail.com>
2019-08-11 01:24:09 +0300

    -microservices changes

--- src/main/java/com/mystic/validation/ValidationRegistration.java
+++ src/main/java/com/mystic/validation/ValidationRegistration.java
@@ -37,3 +37,42 @@
 //    }
 //
 //}
+package com.mystic.validation;
+
+///**
+// * @author Pavel Putrenkov
+// * @version 1.0
+// * @since
+// */
+//@Service
+//@AllArgsConstructor
+//public class ValidationRegistration {
+//
+//    private final UserRepository userRepository;
+//
+//    private ValidationService validationService;
+//
+//    public void validation(String firstname, String username) throws RegistrationException {
+//
+//        if (!validationService.isNameValid(firstname) & !firstname.equals("")) {
+//            throw new RegistrationException(firstname + "Firstname is not valid");
+//        }
+//
+//        if (!validationService.isNameValid(username)) {
+//            throw new RegistrationException("Username is not valid");
+//        }
+//
+//
+//        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+//            String name = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//            if (userRepository.findByUsername(username) != null && !name.equals(username)) {
+//                throw new RegistrationException(username + " User exist");
+//            }
+//        } else {
+//            if (userRepository.findByUsername(username) != null) {
+//                throw new RegistrationException(username + " User exist");
+//            }
+//        }
+//    }
+//
+//}
